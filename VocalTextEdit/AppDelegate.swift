5b7a3e5b1f491110ae737afd3def8cbeb5cee461
Frank Reichard <frankreichard10@gmail.com>
2019-01-06 11:46:36 -0500

    Initial Commit

---
+++ VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by Reichard, Frank (US - Mechanicsburg Delivery) on 1/6/19.
+//  Copyright © 2019 Reichard, Frank (US - Mechanicsburg Delivery). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Reichard, Frank (US - Mechanicsburg Delivery) on 1/6/19.
+//  Copyright © 2019 Reichard, Frank (US - Mechanicsburg Delivery). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
